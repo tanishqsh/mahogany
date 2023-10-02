40e186dfc1663a08bc02b6335505c3ae8354d41c
Tanishq Sharma <tanishqsh@gmail.com>
2023-10-02 15:06:04 -0400

    Updated BarChart with Animated Axis now

--- src/charts/BarChart/BarChart.tsx
+++ src/charts/BarChart/BarChart.tsx
@@ -3,6 +3,7 @@ import '../../tailwind.min.css';
 import React, { FC } from 'react';
 import { BarChartProps } from './barChartTypes';
 import { defaults } from './defaults';
+import { darkenColor } from '../../lib-utils/fx';
 
 const BarChart: FC<BarChartProps> = ({
 	data = defaults.data,
@@ -17,24 +18,54 @@ const BarChart: FC<BarChartProps> = ({
 	const viewBoxWidth = typeof width === 'string' ? 100 : width;
 	const viewBoxHeight = typeof height === 'string' ? 100 : height;
 
+	const axisOffset = 12;
+
 	const maxValue = Math.max(...data.map((item) => item.value));
-	const barWidth = (viewBoxWidth - paddingBetweenBars * (data.length - 1)) / data.length; // Calculate dynamic bar width considering padding
+	const barWidth = (viewBoxWidth - axisOffset - paddingBetweenBars * (data.length - 1)) / data.length; // Recalculate bar width considering padding and axis offset
+
+	/** Calculates the stroke color */
+	const axisStrokeColor = darkenColor(color, 35);
 
 	return (
 		<svg width={width} height={height} viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}>
+			{/** X Axis*/}
+			<motion.line
+				initial={{ x2: 0, opacity: 0 }}
+				animate={{ x2: viewBoxWidth, opacity: 1 }}
+				transition={{ duration: 0.5, delay: data.length * 0.05 + 0.5 }}
+				x1={0}
+				y1={viewBoxHeight}
+				y2={viewBoxHeight}
+				stroke={axisStrokeColor}
+				strokeWidth="4"
+				strokeDasharray="1,3"
+			/>
+
+			{/** Y Axis*/}
+			<motion.line
+				initial={{ y2: viewBoxHeight, opacity: 0 }}
+				animate={{ y2: 0, opacity: 1 }}
+				transition={{ duration: 0.5, delay: data.length * 0.05 + 0.5 }}
+				x1={0}
+				y1={viewBoxHeight}
+				x2={0}
+				stroke={axisStrokeColor}
+				strokeWidth="4"
+				strokeDasharray="1,3"
+			/>
+
 			{data.map((value, index) => {
 				const scaledHeight = (value.value / maxValue) * viewBoxHeight; // Scale height
 
 				return (
 					<motion.rect
 						key={index}
-						x={index * (barWidth + paddingBetweenBars)}
-						y={viewBoxHeight - scaledHeight}
+						x={axisOffset + index * (barWidth + paddingBetweenBars)}
+						y={viewBoxHeight - scaledHeight - axisOffset}
 						width={barWidth}
 						height={scaledHeight}
 						fill={color}
-						rx={5}
-						ry={5}
+						radius={5}
 						initial={{ y: scaledHeight, opacity: 0 }}
 						animate={{ y: 0, opacity: 1 }}
 						transition={{ duration: 0.5, type: 'tween', delay: index * 0.05 }}
