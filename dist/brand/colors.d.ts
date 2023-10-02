74327e4d3fa56b25300a8149800c237d32274a66
Tanishq Sharma <tanishqsh@gmail.com>
2023-10-02 13:33:08 -0400

    Updated the git to ignore dist folder

--- dist/brand/colors.d.ts
+++
@@ -1,2 +0,0 @@
-declare let mahoganyPallete: string[];
-export { mahoganyPallete };
--- dist/brand/colors.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mahoganyPallete = void 0;
-let mahoganyPallete = ['#606C38', '#283618', '#FEFAE0', '#DDA15E', '#BC6C25'];
-exports.mahoganyPallete = mahoganyPallete;
